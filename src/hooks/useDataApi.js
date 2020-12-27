a652fad65d30b2f5698efd86194cd6d97db07a70
Arcadio Pernia <arcadio.perniac@gmail.com>
2020-12-27 15:35:59 -0300

    :sparkles: Create custom hook for fetching data to themoviedb.org API

---
+++ src/hooks/useDataApi.js
@@ -0,0 +1,49 @@
+import {
+    useState,
+    useEffect,
+    useReducer
+  } from "react";
+import axios from 'axios';
+import dataFetchReducer from "./../reducers/dataFetchReducer";
+
+const useDataApi = (initialUrl, initialData) => {
+    const [url, setUrl] = useState(initialUrl);
+
+    const [state, dispatch] = useReducer(dataFetchReducer, {
+        isLoading: false,
+        isError: false,
+        data: initialData,
+    });
+
+    useEffect(() => {
+        let didCancel = false;
+
+        async function fetchData() {
+            dispatch({ type: 'FETCH_INIT' });
+
+            try {
+            const result = await axios(url);
+
+            if (!didCancel) {
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+            }
+            } catch (error) {
+                if (!didCancel) {
+                    dispatch({ type: 'FETCH_FAILURE' });
+                }
+
+            }
+
+        };
+
+        fetchData();
+
+        return () => {
+            didCancel = true;
+        };
+    }, [url]);
+
+    return [state, setUrl];
+};
+
+export default useDataApi;
