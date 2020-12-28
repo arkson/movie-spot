e6ccd879ba5f9c36826446381ab88d1517adb558
Arcadio Pernia <arcadio.perniac@gmail.com>
2020-12-28 18:47:01 -0300

    :construction: Create ModalContext & Modal component

---
+++ src/ModalContext.js
@@ -0,0 +1,16 @@
+import React, { createContext } from "react";
+import useModal from "./hooks/useModal";
+import Modal from "./Modal";
+
+export const ModalContext = createContext();
+
+export const ModalProvider = props => {
+  let { modal, handleModal, modalContent } = useModal();
+  return (
+    <ModalContext.Provider value={{ modal, handleModal, modalContent }}>
+      <Modal />
+      {props.children}
+    </ModalContext.Provider>
+  );
+};
+
